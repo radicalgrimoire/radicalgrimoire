1353921a2f3d6e17f8ac170a663f0bd371a1fa65
ueno.s <ueno.s@gamestudio.co.jp>
2024-04-25 12:07:25 +0900

    不要ファイルの削除

--- helix-auth-svc-build/files/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo -E -u perforce p4d -Gc
-sudo -E -u perforce p4d -Gf
-sudo -E -u perforce p4 trust -y
-sudo -E -u perforce p4p -d -p ${PORT} -t ${P4PORT} -r ${P4PCACHE} -L ${P4PLOGFILE} -v 3
--- p4proxy-build/files/log.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec /usr/bin/tail -f ${P4PLOGFILE}
