7483242f906315a9d771113fa1aa0af00329ae69
ueno.s <ueno.s@gamestudio.co.jp>
2024-07-17 18:12:42 +0900

    other data deleted

--- redmine_issue_badge/spec/factories/issue_statuses.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :issue_status do
-    sequence(:name)     { |n| "status-name: #{n}" }
-    sequence(:position) { |n| n }
-    is_closed { false }
-  end
-end
--- redmine_omniauth_saml/db/migrate/001_add_omniauth_saml_attribute_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddOmniauthSamlAttributeToUser < ActiveRecord::Migration[4.2]
-  def change
-    add_column :users, :created_by_omniauth_saml, :boolean, default: false
-  end
-end
